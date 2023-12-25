6bf99a9b370d595ff4ab09ad62bb1a2d6484f37b
Foxy3355 <sema12301@gmail.com>
2023-12-25 18:15:25 +0700

    Initial commit

---
+++ myrest/library/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import *
+
+routerA = routers.SimpleRouter()
+routerA.register(r'author', AuthorListAndDetailAPI)
+
+routerB = routers.SimpleRouter()
+routerB.register(r'books', BookListAndDetailAPI)
+
+urlpatterns = [
+    path('rest/api/', include(routerA.urls)),
+    path('rest/api/', include(routerB.urls)),
+    path('rest/api/add_book', AddBookAPI.as_view()),
+]
